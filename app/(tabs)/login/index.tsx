5b9902dfd283c32e772ba2385779739d1ab28a07
Thmsantos <thiagomessias411@gmail.com>
2025-03-19 10:49:51 -0300

    :four_leaf_clover: feat: Configurando app para utilização de rotas

---
+++ app/(tabs)/login/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "@/components/Themed";
+
+export default function Login(){
+    return(
+        <View>
+            <Text style={{color: '#000', fontSize: 30}}>Login</Text>
+        </View>
+    )
+}
